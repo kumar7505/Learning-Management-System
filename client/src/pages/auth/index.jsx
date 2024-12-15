7dff447dc89ad08bc2609e39efdfcc0ab6c51c50
kumar7505 <kumarpers7@gmail.com>
2024-12-15 22:58:23 +0530

    feat: header setup

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import {GraduationCap} from "lucide-react";
+import { Link } from "react-router-dom";
+import {Tabs} from "@/components/ui/tabs";
+import { useState } from "react";
 
-const index = () => {
-  return <div>auth</div>;
+const AuthPage = () => {
+  const [activeTab, setActiveTab] = useState("signin");
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="px-4 lg:px-6 h-14 flex items-center border-b">
+        <Link to={"/"} className="flex items-center justify-center">
+          <GraduationCap className="h-8 w-8 mr-4" />
+          <span className="font-extrabold text-xl">LMS LEARN</span>
+        </Link>
+      </header>
+
+      <div className="flex items-center justify-center min-h-screen bg-background">
+        <Tabs
+          value={activeTab}
+          defaultValue="signin"
+          onValueChange={handleTabChange}
+          className="w-full max-w-md">
+
+        </Tabs>
+      </div>
+    </div>
+  );
 };
 
-export default index;
+export default AuthPage;
