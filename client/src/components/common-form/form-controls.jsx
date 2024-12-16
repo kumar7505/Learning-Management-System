29a612d28e5fc78b292b6f6d15138436d4c8729c
kumar7505 <kumarpers7@gmail.com>
2024-12-16 23:00:01 +0530

    chore: render

--- client/src/components/common-form/form-controls.jsx
+++ client/src/components/common-form/form-controls.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 import { Label } from "../ui/label";
 
 function FormControls({formControls = [], fromData, setFormData}){
+  function renderComponentByType(getControlItem) {  
+    let element = null;
+    const currentControlItemValue = formData[getControlItem.name] || "";
+
+    switch (getControlItem.componentType) {
+      case "input":
+        element = (
+          <Input
+            id={getControlItem.name}
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            type={getControlItem.type}
+          />
+      )}
+    }
   return (
     <div className="flex flex-col gap-3">
       {formControls.map((controleItem) => (
         <div key={controleItem.name}>
-          <Label htmlFor={controleItem.name}>{controleItem.label}</Label>
-          {renderComponentByType(controleItem)}
+          <Label htmlFor={controlItem.name}>{controleItem.label}</Label>
+          {renderComponentByType(controlItem)}
         </div>
       ))}
     </div>
