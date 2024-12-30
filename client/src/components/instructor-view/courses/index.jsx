34fb02b611d7d056de3e736f034cce2433422f1f
kumar7505 <kumarpers7@gmail.com>
2024-12-30 13:03:16 +0530

    feat: buttons for instructor

---
+++ client/src/components/instructor-view/courses/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InstructorCourses = () => {
+  return (
+    <div>Instructor Courses</div>
+  )
+}
+
+export default InstructorCourses
---
+++ client/src/components/instructor-view/dashboard/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InstructorDashboard = () => {
+  return (
+    <div>Instructor Dashboard</div>
+  )
+}
+
+export default InstructorDashboard
