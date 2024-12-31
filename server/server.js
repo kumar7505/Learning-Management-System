c7b7f68b199467b13321767ae890a4edf33c5531
kumar7505 <kumarpers7@gmail.com>
2024-12-31 21:48:27 +0530

    completing the course upload for instructor

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const authRoutes = require('./routes/auth-routes/index');
+const mediaRoutes = require('./routes/instructor-routes/media-routes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -26,6 +27,7 @@ mongoose.connect(MONGO_URL)
   .catch((e)=>console.error(e));
 
 app.use('/auth', authRoutes);
+app.use('/media', mediaRoutes);
 
 app.use((err, req, res, next) => {
     console.log(err.stack); // Logs the error stack trace for debugging purposes
