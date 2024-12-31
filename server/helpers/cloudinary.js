40a1ed783cb8d38aeb9f9734a4ceb35851a1e38c
kumar7505 <kumarpers7@gmail.com>
2024-12-31 10:05:50 +0530

    feat: uploading and deleteing media from cloud

---
+++ server/helpers/cloudinary.js
@@ -0,0 +1,29 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_NAME,
+    api_secret : process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadMediaToCloudinary = async(filepath) => {
+    try{
+        const result = await cloudinary.uploader.upload(filePath, {
+            resource_type: 'auto'
+        })
+        return result;
+    } catch(err){
+        console.log(err);
+        throw new Error('Error uploading to cloud');
+    }
+};
+
+const deleteMediaFromCloudinary = async(publicId) => {
+    try {
+        await cloudinary.uploader.destroy(publicId);
+    } catch(err) {
+        throw new Error("Failed to delete assets from cloud");
+    }
+};
+
+module.exports = { uploadMediaToCloudinary, deleteMediaFromCloudinary };
