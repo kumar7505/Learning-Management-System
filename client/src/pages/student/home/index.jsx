a231f89024b7bb83e8c185f4a716a87b8cfdf714
kumar7505 <kumarpers7@gmail.com>
2025-01-15 10:51:43 +0530

    Student page init

--- client/src/pages/student/home/index.jsx
+++ client/src/pages/student/home/index.jsx
@@ -1,24 +1,36 @@
-import StudentViewCommonLayout from '@/components/student-view/common-layout';
-import { Button } from '@/components/ui/button';
-import { AuthContext } from '@/context/auth-context';
-import React, { useContext } from 'react'
-
+import React from 'react'
+import banner from '../../../assets/banner-img.png'
+import { courseCategories } from '@/config'
+import { Button } from '@/components/ui/button'
 const StudentHomePage = () => {
-  const {resetCredentials} = useContext(AuthContext);
-
-  function handleLogout() {
-    resetCredentials();
-    sessionStorage.clear();
-  }
-
+  
   return (
-    <div>
-      <StudentViewCommonLayout />
-      Student Home Page
-
-      <Button onClick={handleLogout}> Logout </Button>
+    <div className="min-h-screen bg-white">
+      <section className="flex flex-xol lg:flex-row items-center justify-between py-8 px-4 lg:px-8">
+        <div className="lg:width-1/2 lg:pr-12">
+          <h1 className="text-4xl font-bold mb-4">Learning that get you</h1>
+          <p className='text-xl'>Skills for your present and your future. get started with US</p>
+        </div>
+        <div className="lg:w-full m-8 lg:mb-0">
+          <img 
+           src={banner} 
+           alt="banner"
+           width={600}
+           height={400}
+           className='w-full h-auto rounded-lg shadow-lg' />
+        </div>
+      </section>
+      <section className="py-8 px-4 lg:px-8 bg:gray-100">
+        <h2 className="text-2xl font-bold mb-6">Course Categories</h2>
+        <div className="grid grid-cols-2 sm:grid-col-3 md:grid-cols-4">
+          {
+            courseCategories.map(categoryItem => 
+              <Button className="justify-start" variant="outline" key={categoryItem.id}>{categoryItem.label}</Button>
+            )
+          }
+        </div>
+      </section>
     </div>
-  )
-}
+)}
 
 export default StudentHomePage
