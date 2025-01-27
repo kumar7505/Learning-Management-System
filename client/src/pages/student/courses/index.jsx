3a4adffdb3d6262a1d966c1c421d522750561292
Kumar P <161574943+kumar7505@users.noreply.github.com>
2025-01-27 21:43:36 +0530

    feat: displaying the content variations

--- client/src/pages/student/courses/index.jsx
+++ client/src/pages/student/courses/index.jsx
@@ -1,18 +1,45 @@
-import { Button } from '@/components/ui/button';
-import { sortOptions } from '@/config';
+import { Button, } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { Checkbox } from '@/components/ui/checkbox';
+import { filterOptions, sortOptions } from '@/config';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuRadioGroup, DropdownMenuRadioItem } from '@radix-ui/react-dropdown-menu';
+import {  } from '@radix-ui/react-label';
 import { ArrowUpDownIcon } from 'lucide-react';
 import React, { useState } from 'react';
 
 const StudentViewCoursesPage = () => {
     const [sort, setSort] = useState('');
+    console.log(filterOptions);
+    const handleFilterOnChange = (keyItem, optionId) => {
+        console.log(`Filter changed for ${keyItem}: ${optionId}`);
+    };
+    
   return (
     <div className='conayiner mx-auto p-4'>
         <h1 className="text-3xl font-bold mb-4">All Courses</h1>
         <div className="flex flex-col md:flex-row gap-4">
             <aside className="w-full md:w-64 space-y-4">
                 <div className="p-4 space-y-4">
-                    filters
+                    {
+                        Object.keys(filterOptions).map((keyItem) => (
+                            <div className="p-4 space-y-4">
+                                <h3 className='font-bold mb-3'>{keyItem.toUpperCase()}</h3>
+                                <div className="grid gap-2 mt-2">
+                                    {
+                                        filterOptions[keyItem].map(option => (
+                                            <Label className='flex font-medium items-center gap-3'>
+                                                <Checkbox 
+                                                    checked={false}
+                                                    onCheckedChange={() => handleFilterOnChange(keyItem, option.id)} 
+                                                />
+                                                {option.label}
+                                            </Label>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </aside>
             <main className="flex-1">
